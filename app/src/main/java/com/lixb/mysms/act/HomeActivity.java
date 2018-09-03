61fd62e3e15c18d3678adc12b015acff16ab376d
lixiaobin <928060035@qq.com>
2018-09-03 08:25:53 +0800

    day day up

---
+++ app/src/main/java/com/lixb/mysms/act/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.lixb.mysms.act;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lixb.mysms.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+    }
+}
